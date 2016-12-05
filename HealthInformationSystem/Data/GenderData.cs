d323e186ed6449433ceffd0fd5d2fd2ce1162dd3
salzheimer <salzheimer@medicalteams.org>
2016-12-05 13:40:21 -0800

    Add ability to pull lists for set fields in office visit

---
+++ HealthInformationSystem/Data/GenderData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthInformationProgram.Data
+{
+    public class GenderData:BaseHipData
+    {
+        public List<Models.GenderModel> GetAll()
+        {
+            var genderList = new List<Models.GenderModel>();
+            var repo = new Data.Repositories.LookupGenderRepository();
+            var dataList =repo.GetAll();
+
+            foreach (var gender in dataList)
+            {
+                var g = new Models.GenderModel();
+
+                g.GenderCode = gender.gndr_cd;
+                g.GenderDescription = gender.gndr_descn;
+                g.GenderId = GetDataValue(gender.gndr_id);
+                g.SortOrder = GetDataValue(gender.user_intrfc_sort_ord);
+
+                genderList.Add(g);
+            }
+            return genderList;
+
+        }
+    }
+}
