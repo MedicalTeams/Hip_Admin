c5ea5a8c85e69657a100001f0467c4285bd332ac
salzheimer <salzheimer@medicalteams.org>
2016-12-09 08:24:01 -0800

    added revisit to office visit create and update in data access

--- HealthInformationSystem/Data/OfficeVisitData.cs
+++ HealthInformationSystem/Data/OfficeVisitData.cs
@@ -98,6 +98,7 @@ namespace HealthInformationProgram.Data
             dataOfficeVisit.infnt_age_mos = (decimal)officeVisit.Age;
             dataOfficeVisit.opd_id = (decimal)officeVisit.OpdId;
             dataOfficeVisit.ov_id = officeVisit.OfficeVisitId;
+            dataOfficeVisit.rvisit_id = officeVisit.RevisitId.Value;
             dataOfficeVisit.staff_mbr_name = officeVisit.StaffMemberName;
             dataOfficeVisit.rec_updt_dt = DateTime.UtcNow;
             dataOfficeVisit.rec_updt_user_id_cd = SessionData.Current.LoggedInUser.UserName;
@@ -116,6 +117,7 @@ namespace HealthInformationProgram.Data
             dataOfficeVisit.infnt_age_mos = (decimal)officeVisit.Age;
             dataOfficeVisit.opd_id = (decimal)officeVisit.OpdId;
             dataOfficeVisit.staff_mbr_name = officeVisit.StaffMemberName;
+            dataOfficeVisit.rvisit_id = officeVisit.RevisitId.Value;
             dataOfficeVisit.rec_updt_dt = DateTime.UtcNow;
             dataOfficeVisit.rec_updt_user_id_cd = SessionData.Current.LoggedInUser.UserName;
             dataOfficeVisit.rec_creat_dt = DateTime.UtcNow;
